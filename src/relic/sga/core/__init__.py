7db8c3d3fb18c2c873169bc139ceff471c4c6348
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-15 11:42:13 -0700

    MAK-Relic-Tool/Issue-Tracker#39 Hotfix
    Fix makedirs not respecting `\\` seperator.

--- src/relic/sga/core/__init__.py
+++ src/relic/sga/core/__init__.py
@@ -3,4 +3,4 @@ Shared definitions used by several components of the module
 """
 from relic.sga.core.definitions import Version, MagicWord, StorageType, VerificationType
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
