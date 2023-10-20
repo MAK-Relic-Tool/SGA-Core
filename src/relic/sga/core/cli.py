cf267f86dfc39267930a2648ebd146dc3c91aa61
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-19 18:39:48 -0700

    Add info cli

--- src/relic/sga/core/cli.py
+++ src/relic/sga/core/cli.py
@@ -3,18 +3,22 @@ from __future__ import annotations
 import argparse
 import os.path
 from argparse import ArgumentParser, Namespace
-from typing import Optional, Callable
+from typing import Optional, Callable, Dict, List, Any, Tuple, Set, TextIO
 
 import fs.copy
 from fs.base import FS
+from fs.multifs import MultiFS
 from relic.core.cli import CliPluginGroup, _SubParsersAction, CliPlugin
+
+from relic.sga.core.definitions import StorageType
+from relic.sga.core.filesystem import EssenceFS, _EssenceDriveFS
 
 
 class RelicSgaCli(CliPluginGroup):
     GROUP = "relic.cli.sga"
 
     def _create_parser(
-        self, command_group: Optional[_SubParsersAction] = None
+            self, command_group: Optional[_SubParsersAction] = None
     ) -> ArgumentParser:
         if command_group is None:
             return ArgumentParser("sga")
@@ -60,7 +64,7 @@ def _get_file_type_validator(exists: Optional[bool]) -> Callable[[str], str]:
 
 class RelicSgaUnpackCli(CliPlugin):
     def _create_parser(
-        self, command_group: Optional[_SubParsersAction] = None
+            self, command_group: Optional[_SubParsersAction] = None
     ) -> ArgumentParser:
         parser: ArgumentParser
         if command_group is None:
@@ -99,7 +103,7 @@ class RelicSgaPackCli(CliPluginGroup):
     GROUP = "relic.cli.sga.pack"
 
     def _create_parser(
-        self, command_group: Optional[_SubParsersAction] = None
+            self, command_group: Optional[_SubParsersAction] = None
     ) -> ArgumentParser:
         parser: ArgumentParser
         if command_group is None:
@@ -118,7 +122,7 @@ class RelicSgaRepackCli(CliPluginGroup):
     GROUP = "relic.cli.sga.repack"
 
     def _create_parser(
-        self, command_group: Optional[_SubParsersAction] = None
+            self, command_group: Optional[_SubParsersAction] = None
     ) -> ArgumentParser:
         parser: ArgumentParser
         desc = "Debug Command; reads and repacks an SGA archive."
@@ -130,3 +134,109 @@ class RelicSgaRepackCli(CliPluginGroup):
         # pack further delegates to version plugins
 
         return parser
+
+
+class RelicSgaInfoCli(CliPlugin):
+    def _create_parser(
+            self, command_group: Optional[_SubParsersAction] = None
+    ) -> ArgumentParser:
+        parser: ArgumentParser
+        description = "Dumps metadata packed into an SGA file."
+        if command_group is None:
+            parser = ArgumentParser("info", description=description)
+        else:
+            parser = command_group.add_parser("info", description=description)
+
+        parser.add_argument(
+            "sga",
+            type=_get_file_type_validator(exists=True),
+            help="SGA File to inspect",
+        )
+        parser.add_argument(
+            "log_file",
+            nargs="?",
+            type=_get_file_type_validator(exists=True),
+            help="Optional file to write messages to",
+            default=None
+        )
+        parser.add_argument("-q", "--quiet", action="store_true", default=False)
+        return parser
+
+    def command(self, ns: Namespace) -> Optional[int]:
+        sga: str = ns.sga
+        log_file: str = ns.log_file
+        quiet: bool = ns.quiet
+
+        logger:Optional[TextIO] = None
+        try:
+            if log_file is not None:
+                logger = open(log_file, "w")
+
+            outputs:List[Optional[TextIO]] = []
+            if quiet is False:
+                outputs.append(None) # None is a sentinel for stdout
+            if logger is not None:
+                outputs.append(logger)
+
+            def _print(*msg:str, sep: Optional[str] = None, end:Optional[str] = None) -> None:
+                for output in outputs:
+                    print(*msg, sep=sep, end=end, file=output)
+
+            def _is_container(d: Any) -> bool:
+                return isinstance(d, (Dict, List, Tuple, Set)) # type: ignore
+
+            def _stringify(d: Any, indent: int = 0) -> None:
+                _TAB = "\t"
+                if isinstance(d, Dict):
+                    for k, v in d.items():
+                        if _is_container(v):
+                            _print(f"{_TAB * indent}{k}:")
+                            _stringify(v, indent + 1)
+                        else:
+                            _print(f"{_TAB * indent}{k}: {v}")
+                elif isinstance(d, (List, Tuple, Set)): # type: ignore
+                    _print(f"{_TAB * indent}{', '.join(*d)}")
+                else:
+                    _print(f"{_TAB * indent}{d}")
+
+            def _getessence(fs: FS, path: str = "/") -> Dict[str,Any]:
+                return fs.getinfo(path, "essence").raw.get("essence", {}) # type: ignore
+
+            _print(f"File: `{sga}`")
+            sgafs: EssenceFS
+            with fs.open_fs(f"sga://{sga}") as sgafs: # type: ignore
+                print("Archive Metadata:")
+                _stringify(sgafs.getmeta("essence"))
+
+                drive:_EssenceDriveFS
+                for alias, drive in sgafs.iterate_fs(): # type: ignore
+                    print(f"Drive: `{drive.name}` (`{drive.alias}`)")
+                    print("\tDrive Metadata:")
+                    info = _getessence(drive)
+                    if len(info) > 0:
+                        _stringify(info, indent=2)
+                    else:
+                        _print(f"\t\tNo Metadata")
+
+                    print("\tDrive Files Metadata:")
+                    for f in drive.walk.files():
+                        print(f"\t\t`{f}`:")
+                        finfo:Dict[str,Any] = _getessence(drive, f)
+                        finfo = finfo.copy()
+                        # We alter storage_type cause it *should* always be present, if its not, we dont do anything
+                        storage_type_key = "storage_type"
+                        if storage_type_key in finfo:
+                            v:int = finfo[storage_type_key]
+                            st:StorageType = StorageType(v)
+                            finfo["storage_type"] = f"{v} ({st.name})"
+
+                        if len(finfo) > 0:
+                            _stringify(finfo, indent=3)
+                        else:
+                            _print(f"\t\t\tNo Metadata")
+
+        finally:
+            if logger is not None:
+                logger.close()
+
+        return None
