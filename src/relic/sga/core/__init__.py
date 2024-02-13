9db04fd80576a557944e11ff9a8ee41b63a487f2
Marcus Kertesz <ModernMAK@Hotmail.com>
2024-02-13 14:22:08 -0800

    build(version): Fix version to match current

--- src/relic/sga/core/__init__.py
+++ src/relic/sga/core/__init__.py
@@ -1,6 +1,4 @@
-"""
-Shared definitions used by several components of the module
-"""
+"""Shared definitions used by several components of the module."""
 
 from relic.sga.core.definitions import (
     Version,
@@ -9,4 +7,4 @@ from relic.sga.core.definitions import (
     VerificationType,
 )
 
-__version__ = "2.0.0.1a"
+__version__ = "1.1.4"
